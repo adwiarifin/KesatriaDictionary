eeea5dab0958161f01f3555816713be2e3228cbe
Adwi Arifin <adwiarifin@gmail.com>
2018-09-11 15:57:54 +0700

    fill in wordadapter

--- app/src/main/java/com/kesatriakeyboard/kesatriadictionary/adapter/WordAdapter.java
+++ app/src/main/java/com/kesatriakeyboard/kesatriadictionary/adapter/WordAdapter.java
@@ -1,4 +1,70 @@
 package com.kesatriakeyboard.kesatriadictionary.adapter;
 
-public class WordAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.kesatriakeyboard.kesatriadictionary.R;
+import com.kesatriakeyboard.kesatriadictionary.model.WordModel;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WordAdapter extends RecyclerView.Adapter<WordAdapter.WordViewHolder> {
+
+    private ArrayList<WordModel> mData = new ArrayList<>();
+    private Context context;
+
+    public WordAdapter(Context context) {
+        this.context = context;
+    }
+
+    public void setData(ArrayList<WordModel> items) {
+        mData = items;
+        notifyDataSetChanged();
+    }
+
+    public WordModel getModel(int position) {
+        if (mData != null) {
+            return mData.get(position);
+        }
+        return null;
+    }
+
+    @Override
+    public WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_word_row, parent, false);
+        return new WordViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        WordModel model = mData.get(position);
+
+        holder.tvWord.setText(model.getWord());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mData != null) {
+            return mData.size();
+        }
+        return 0;
+    }
+
+    public static class WordViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.textview_word)
+        TextView tvWord;
+
+        public WordViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 }
