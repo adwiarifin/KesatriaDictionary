472a7d06bc3bf40704f3fdbf940a9328df5b1911
Adwi Arifin <adwiarifin@gmail.com>
2018-09-12 14:54:01 +0700

    fill in SplashActivity

--- app/src/main/java/com/kesatriakeyboard/kesatriadictionary/view/SplashActivity.java
+++ app/src/main/java/com/kesatriakeyboard/kesatriadictionary/view/SplashActivity.java
@@ -1,15 +1,59 @@
 package com.kesatriakeyboard.kesatriadictionary.view;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.kesatriakeyboard.kesatriadictionary.R;
+import com.kesatriakeyboard.kesatriadictionary.prefs.AppPreference;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SplashActivity extends AppCompatActivity {
+
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        new LoadData().execute();
+    }
+
+    private class LoadData extends AsyncTask<Void, Integer, Void>{
+
+        final String TAG = LoadData.class.getSimpleName();
+//        MahasiswaHelper mahasiswaHelper;
+        AppPreference appPreference;
+        double progress;
+        double maxprogress = 100;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Intent i = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(i);
+            finish();
+        }
     }
 }
